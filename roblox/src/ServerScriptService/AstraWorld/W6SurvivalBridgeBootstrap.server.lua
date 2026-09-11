7977eafd1ea2a23a0e07e2d31b66e32eb3774add
nustanakritwithai <nustanakritwithai@gmail.com>
2026-09-12 03:15:27 +0700

    refactor(integration): remove parallel W6 bootstrap

--- roblox/src/ServerScriptService/AstraWorld/W6SurvivalBridgeBootstrap.server.lua
+++
@@ -1,7 +0,0 @@
-local SurvivalBridgeService = require(script.Parent.SurvivalBridgeService)
-
-local result = SurvivalBridgeService.Start()
-print(string.format(
-    "[AstraLife][W6] survival bridge online status=%s",
-    tostring(result.runtime.state:GetAttribute("W6Status"))
-))
