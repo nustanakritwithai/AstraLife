79d61d000db500725a734aa647b1caa47b9e0754
nustanakritwithai <nustanakritwithai@gmail.com>
2026-09-12 03:15:40 +0700

    refactor(integration): remove parallel W7 bootstrap

--- roblox/src/ServerScriptService/AstraWorld/W7EcosystemBootstrap.server.lua
+++
@@ -1,7 +0,0 @@
-local EcosystemService = require(script.Parent.EcosystemService)
-
-local result = EcosystemService.Start()
-print(string.format(
-    "[AstraLife][W7] ecosystem online status=%s",
-    tostring(result.runtime.state:GetAttribute("W7Status"))
-))
